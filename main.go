9a2efbaa0baf843b729b09c86f9dd34a767e5d76
stoffs <st0yanoff.stoyan@gmail.com>
2017-02-07 18:11:41 +0100

    added what to be deployed

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"expvar"
+	"log"
+	"net/http"
+)
+
+var (
+	redirects = expvar.NewInt("redirects")
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://welt.de", http.StatusMovedPermanently)
+	redirects.Add(1)
+	log.Printf("Redirected...")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9090", nil)
+}
