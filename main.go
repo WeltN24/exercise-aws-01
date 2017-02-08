3639f63f15b3a8b8529d7be106eec423dd62bfe1
stoffs <st0yanoff.stoyan@gmail.com>
2017-02-08 10:53:07 +0100

    changed routes added health route

--- main.go
+++ main.go
@@ -10,13 +10,19 @@ var (
 	redirects = expvar.NewInt("redirects")
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func redirect(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "https://welt.de", http.StatusMovedPermanently)
 	redirects.Add(1)
 	log.Printf("Redirected...")
 }
 
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/redirecter/", redirect)
+	http.HandleFunc("/redirecter/health", health)
+
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
