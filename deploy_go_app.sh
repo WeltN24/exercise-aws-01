98198d2b4ef8f7aa7a064d644f0fc92203577900
cristidas <cristi.dascalu@gmail.com>
2017-04-11 01:40:00 +0200

    Create deploy_go_app.sh

---
+++ deploy_go_app.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# Due to basic requirements for building this environment we are choosing the AWS Elastic Beanstalk service:
+# - Go application - nginx based reverse proxy maps the application to the load balancer
+# - highly available - will deploy AWS Elasti Load Balancer and EC2 instances in Auto Scaling Group
+# - log aggregation - AWS CloudWatch Logs
+
+# Create the application home directory
+
+# Initialize the git repository
+
+# Fetch from remote repository and integrate local
+
+# Commit changes to the local repository
+
+# Configure the AWS EB CLI and the project
+
+# Create the AWS Elastic Beanstalk environment
+
+# Enable AWS CloudWatch logs
