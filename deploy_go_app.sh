664c8b5a027ab3eae40ae693c2348ef2cfe02e16
cristidas <cristi.dascalu@gmail.com>
2017-04-11 01:45:40 +0200

    Update deploy_go_app.sh

--- deploy_go_app.sh
+++ deploy_go_app.sh
@@ -1,20 +1,37 @@
 #! /bin/sh
 
-# Due to basic requirements for building this environment we are choosing the AWS Elastic Beanstalk service:
+# Due to basic requirements for building this environment we are choosing the AWS Elastic Beanstalk service
+# (and deploying it via AWS EB CLI):
 # - Go application - nginx based reverse proxy maps the application to the load balancer
 # - highly available - will deploy AWS Elasti Load Balancer and EC2 instances in Auto Scaling Group
 # - log aggregation - AWS CloudWatch Logs
 
 # Create the application home directory
 
+mkdir welt24go
+cd welt24go
+
 # Initialize the git repository
+
+sudo git init
 
 # Fetch from remote repository and integrate local
 
+sudo git pull https://github.com/cristidas/exercise-aws-01.git
+
 # Commit changes to the local repository
+
+sudo git commit "Initial commit"
 
 # Configure the AWS EB CLI and the project
 
+eb init -p "Go 1.6" -r us-west-2 w24new
+
 # Create the AWS Elastic Beanstalk environment
 
+eb create w24newenv -d --elb-type classic
+
 # Enable AWS CloudWatch logs
+
+eb logs -cw
+
